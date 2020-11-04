67c73c9986aac4ff1b95ee572c7dbe15ec689bec
han <2397075084@qq.com>
2020-11-04 14:32:44 +0800

    first commit

---
+++ app/Http/Controllers/WX/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\WX;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    //
+
+
+    public function  index(){
+echo "杨楠吃屎";
+
+
+    }
+    public function list(){
+
+    }
+
+
+}
