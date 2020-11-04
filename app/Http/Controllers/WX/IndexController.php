4c0cc21aea7737e4e17ead47da1d0ecf618a3386
han <2397075084@qq.com>
2020-11-04 20:07:36 +0800

    .env

--- app/Http/Controllers/WX/IndexController.php
+++ app/Http/Controllers/WX/IndexController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers\WX;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use DB;
+use Illuminate\Support\Facades\Redis;
 class IndexController extends Controller
 {
     //
 
 
     public function  index(){
-echo "杨楠吃屎";
+    	// $data=["name"=>"LZY","pwd"=>123456];
 
+    	// $res=DB::table("user")->insert($data);
+    	// if($res){
+    	// $res=	DB::table("user")->get();
+    	// 	dd($res);
+
+    	//}
+		//echo "杨楠吃屎";
+        Redis::set("lzy","ok");
+        echo    Redis::get("lzy");
 
     }
     public function list(){
