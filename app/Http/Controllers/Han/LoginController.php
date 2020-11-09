95ee406f74b5c4948dbffd2219d6129849294335
han <2397075084@qq.com>
2020-11-09 15:55:24 +0800

    asd

--- app/Http/Controllers/Han/LoginController.php
+++ app/Http/Controllers/Han/LoginController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Han;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Model\LoginModel;//LoginModel
+use Illuminate\Support\Facades\Cookie;
 class LoginController extends Controller
 {
     //
@@ -14,8 +15,41 @@ class LoginController extends Controller
 				return view("Login/login");
 			}
 			if(request()->isMethod("post")){
+				  $wetch_user = request()->wetch_user;
 
-			}
+				   $wetch_pwd = request()->wetch_pwd;
+			
+				   $rember = [
+			
+					 'wetch_user'=>$wetch_user,
+					 'wetch_pwd'=>$wetch_pwd
+			
+				   ];
+	  				if(empty($wetch_user)){
+				        error("用户名不能为空!"); 
+				   	}
+				    if(empty($wetch_pwd)){
+				   
+				        error("密码不能为空!"); 
+				    }
+			
+				  $login=LoginModel::where('wetch_user',$wetch_user)->first();
+				  if(!$login){
+				     error("用户名或者密码错误!");
+				  }
+				  if(password_verify($wetch_pwd,$login['wetch_pwd'])){
+					   if(isset(request()->rember)){
+ 			                   Cookie::queue('rember',serialize($rember),60*24*7);
+ 			           }
+					   session(['users'=>$rember]);
+ 			          success("登陆成功");
+				}else{
+				       error("用户名或者密码错误!");
+				}
+			
+			
+			
+		}
 
 
 		}
--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 <head>
@@ -5,16 +6,17 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 <title>登录</title>
-<link rel="stylesheet" type="text/css" href="logins/normalize.css" />
-<link rel="stylesheet" type="text/css" href="logins/demo.css" />
+<link rel="stylesheet" type="text/css" href="/logins/normalize.css" />
+<link rel="stylesheet" type="text/css" href="/logins/demo.css" />
 
 <!--必要样式-->
-<link rel="stylesheet" type="text/css" href="logins/component.css" />
-<!-- // --><link rel="stylesheet" type="text/css" href="logins/download.jpg" />
-<!--  <img src="logins/download.jpg" alt="">
- --><!--[if IE]>
+<link rel="stylesheet" type="text/css" href="/logins/component.css" />
+<!-- // --><link rel="stylesheet" type="text/css" href="/logins/demo-1-bg.jpg" />
+<!--[if IE]>
 <script src="js/html5.js"></script>
 <![endif]-->
+<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<!-- 表单提示错误信息 手册第321页-322页-->
@@ -25,32 +27,59 @@
 		<div id="large-header" class="large-header">
 			<canvas id="demo-canvas"></canvas>
 			<div class="logo_box">
-				<h3>微商城后台登陆</h3>
+				<h3>微信后台登陆</h3>
 				<form action="http://admin.1912.com/login/store" name="f" method="post">
 					<input type="hidden" name="_token" value="mseqeQpBTsmT57a70lgVWEuVxgBgAsnGgG8psbvl">					<div class="input_outer">
 						<span class="u_user"></span>
-						<input name="admin_name" class="text" style="color: #FFFFFF !important" type="text" placeholder="请输入账户">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-						<b><font color=red></font></b>
+						<input name="wetch_user" class="text" style="color: #FFFFFF !important" type="text" placeholder="请输入账户">
 					</div>
 					<div class="input_outer">
 						<span class="us_uer"></span>
-						<input name="admin_pwd" class="text" style="color: #FFFFFF !important; position:absolute; z-index:100;"value="" type="password" placeholder="请输入密码">
-					</div>	&nbsp;&nbsp;
-					<input name="rember" type="checkbox" />
+						<input name="wetch_pwd" class="text" style="color: #FFFFFF !important; position:absolute; z-index:100;"value="" type="password" placeholder="请输入密码">
+					</div>
+					<input name="rember" value="1" type="checkbox" />
     					<span>七天免登录</span>
-					
-				
-					<button class="act-but submit" type="submit" style="color: #FFFFFF">登录</button>
-					<!-- <input  class="act-but submit"  style="color: #FFFFFF" value="登录"> -->
-					
+				    <center>
+					     <p><font color="red" id="b"></font></p>
+					     <button class="act-but submit" type="button" id="button" style="color: #FFFFFF">登录</button>
+					</center>
 				</form>
 			</div>
 		</div>
 	</div>
 </div><!-- /container -->
-		<script src="logins/TweenLite.min.js"></script>
-		<script src="logins/EasePack.min.js"></script>
-		<script src="logins/rAF.js"></script>
-		<script src="logins/demo-1.js"></script>
+		<script src="/logins/TweenLite.min.js"></script>
+		<script src="/logins/EasePack.min.js"></script>
+		<script src="/logins/rAF.js"></script>
+		<script src="/logins/demo-1.js"></script>
 	</body>
 </html>
+<script>
+$(document).on("click","#button",function(){
+
+  var wetch_user = $("input[name='wetch_user']").val();
+  var wetch_pwd = $("input[name='wetch_pwd']").val();
+  var rember = $("input[name='rember']:checked").val();
+  if(wetch_user==''){
+        $("input[name='wetch_user']").prop("placeholder","账号不能为空!");return;
+  }
+   if(wetch_pwd==''){
+        $("input[name='wetch_pwd']").prop("placeholder","密码不能为空!");return;
+  }
+  $.ajax({
+	    url:"{{url('/login')}}",
+        type:'post',
+		dataType:'json',
+		data:{wetch_user:wetch_user,wetch_pwd:wetch_pwd,rember:rember},
+		async:true,
+		success:function(res){
+			if(res.error_no=='0'){
+				alert(res.error_msg);
+			    location.href="{{url('/list')}}";
+			}else if(res.error_no=='1'){
+			   $("#b").text(res.error_msg);
+		   }
+		}
+  });
+});
+</script>
