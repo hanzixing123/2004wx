a58a85e4f5ef69b6682dca88aa674140a81e9c6a
han <2397075084@qq.com>
2020-11-09 16:02:30 +0800

    ASD

--- app/Model/LoginModel.php
+++ app/Model/LoginModel.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class LoginModel extends Model
 {
     //
+	    protected $table ='wetch_login';
+
+    protected  $primaryKey ='wetch_id';
+
+    public $timestamps=false;
+
+// 黑名单属性
+    protected $guarded=[];
+
+
+
+
+
+
+
 }
